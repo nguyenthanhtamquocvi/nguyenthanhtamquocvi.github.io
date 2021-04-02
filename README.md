0b423a6e16b2bc21d7c2a6c8670586963ab28b8e
quocvi <50450222+nguyenthanhtamquocvi@users.noreply.github.com>
2021-04-02 19:08:32 +0700

    fef
    sedfe

--- README.md
+++
@@ -1,1 +0,0 @@
-# nguyenthanhtamquocvi.github.io
--- scrript/load.js
+++
@@ -1,6 +0,0 @@
-// JavaScript Document
-$(window).on('load', function(event))
-{
-	$('body').removeClass('preloading');
-	$('.bod').delay(1000).fadeOut('fast');
-}
